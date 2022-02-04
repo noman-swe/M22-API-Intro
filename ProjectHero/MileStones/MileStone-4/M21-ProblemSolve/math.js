2ce138f0f60574dc0ead539b254e619511f42181
noman-swe <noman.swe130@gmail.com>
2022-02-05 02:33:30 +0600

    concept checklist

---
+++ ProjectHero/MileStones/MileStone-4/M21-ProblemSolve/math.js
@@ -0,0 +1,16 @@
+/*
+const myNumber = -5;
+const output = Math.abs(myNumber);
+console.log(output);
+*/
+
+const myNumber = 6.700474;
+//const output = Math.ceil(myNumber); //output: 7 //uporer diker full integer ta k nibe
+//const output = Math.floor(myNumber); //output: 6 // nichyr diker full integer ta k nibe
+
+//const output = Math.round(myNumber);// round() will take to nearest integer .5 or beshi kishu hooile net integer ta output ashbe
+
+
+//random digit
+const output = Math.random(myNumber);
+console.log(output * 10);
