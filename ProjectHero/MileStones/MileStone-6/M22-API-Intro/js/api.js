65d4d574de32994334e9f2180c66a917e67738b8
noman-swe <noman.swe130@gmail.com>
2022-02-26 22:28:15 +0600

    33-4 Using API Load more data, send data to function

--- ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/api.js
+++ ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/api.js
@@ -4,8 +4,27 @@ function loadData() {
         .then(json => console.log(json))
 }
 
-function loadDatas (){
+function loadDatas() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
+        .then(response => response.json())
+        .then(json => console.log(json));
+}
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUsers(data))
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+// loadPosts()
+
+function displayUsers(data){
+    for(const element of data){
+        console.log(element.name);
+    }
 }
