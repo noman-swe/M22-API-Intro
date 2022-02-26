75e9a1460a29586a4be748f29fc4232aad1eca1e
noman-swe <noman.swe130@gmail.com>
2022-02-26 06:12:04 +0600

    33-1 Module Introduction and What is an API

--- ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/index.js
+++ ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/index.js
@@ -1,3 +1,6 @@
 document.getElementById('hlw').addEventListener('click', function() {
 this.innerText = ('hlw Api');
-});
+});  
+
+const user = { id:11, name: 'Girob amir', job: 'actor' };
+console.log(user);
