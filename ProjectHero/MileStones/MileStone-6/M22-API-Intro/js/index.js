fb9c035668915b07d0d16eb467742965ce620b00
noman-swe <noman.swe130@gmail.com>
2022-02-26 08:47:42 +0600

    33-2 Intro to JSON, JSON Structure, parse, stringify

--- ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/index.js
+++ ProjectHero/MileStones/MileStone-6/M22-API-Intro/js/index.js
@@ -3,4 +3,24 @@ this.innerText = ('hlw Api');
 });  
 
 const user = { id:11, name: 'Girob amir', job: 'actor' };
+const stringify = JSON.stringify(user);
 console.log(user);
+console.log(stringify);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir Road',
+    profit: '15000',
+    products: ['laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'Alia',
+    },
+    isExpensive: false
+}
+// console.log(shop);
+const shopStringify = JSON.stringify(shop);
+console.log(shopStringify);
+
+// stringify k abr normal javascript object e change kora jay
+const converted = JSON.parse(shopStringify);
+console.log(converted.owner);
